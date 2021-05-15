ac5e0f98a129802993c847f79a73b18aa4be02c3
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-05-15 22:06:13 +0100

    Add text

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,12 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import kotlin.math.min
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -15,19 +19,34 @@ class LoadingButton @JvmOverloads constructor(
 
     private val valueAnimator = ValueAnimator()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) {
-            p, old, new ->
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
     }
 
+    private var radius = 0.0f
+    private val textPosition: PointF = PointF(0.0f, 0.0f)
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textAlign = Paint.Align.CENTER
+        textSize = resources.getDimension(R.dimen.default_text_size)
+    }
+
+    private fun PointF.calculateTextPosition() {
+        x = width / 2.0f
+        y = height / 2.0f + resources.getDimension(R.dimen.loading_button_text_size) / 2.0f
+    }
 
     init {
 
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        textPosition.calculateTextPosition()
+        radius = (height / 2.0f * 0.8).toFloat()
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
+        canvas?.drawText("Loading button", textPosition.x, textPosition.y, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
