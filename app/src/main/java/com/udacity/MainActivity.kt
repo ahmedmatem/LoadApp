2f8a93a4c58b36ae939c80fcd6ec703646970528
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-05-16 16:07:49 +0100

    Add image and radio buttons group

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -38,25 +41,52 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+//            Toast.makeText(context, "id: $id", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+        if (radio_group.checkedRadioButtonId == -1) {
+            Toast.makeText(this, R.string.radio_group_unchecked_toast_text, Toast.LENGTH_LONG)
+                .show()
+        } else {
+            val request =
+                DownloadManager.Request(Uri.parse(URL))
+                    .setTitle(getString(R.string.app_name))
+                    .setDescription(getString(R.string.app_description))
+                    .setRequiresCharging(false)
+                    .setAllowedOverMetered(true)
+                    .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadID =
+                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+            when (view.id) {
+                R.id.radio_glide ->
+                    if (checked) {
+                        URL = "https://github.com/bumptech/glide"
+                    }
+                R.id.radio_app ->
+                    if (checked) {
+                        URL =
+                            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+                    }
+                R.id.radio_retrofit ->
+                    if (checked) {
+                        URL = "https://github.com/square/retrofit"
+                    }
+            }
+        }
     }
 
     companion object {
-        private const val URL =
+        private var URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
