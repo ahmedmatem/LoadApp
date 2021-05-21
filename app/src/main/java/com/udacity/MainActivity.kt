67fd0affdb2587b4d076aab9c0439cb089932151
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-05-21 19:51:58 +0100

    Add custom notification style - bigPictureStyle

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
@@ -33,7 +36,12 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(CHANNEL_ID, getString(R.string.channel_name))
+
         custom_button.setOnClickListener {
+            // clear previous notification from status bar
+            notificationManager.cancelAll()
             download()
         }
     }
@@ -42,7 +50,59 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 //            Toast.makeText(context, "id: $id", Toast.LENGTH_LONG).show()
+            notificationManager.sendNotification(context!!)
         }
+    }
+
+    private fun createNotificationChannel(channelId: String, name: String) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_DEFAULT)
+                    .also {
+                        it.description = getString(R.string.notification_description)
+                        it.enableLights(true)
+                        it.lightColor = getColor(R.color.colorAccent)
+                        it.enableVibration(true)
+                    }
+            // Register the channel with the system
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun NotificationManager.sendNotification(appContext: Context) {
+        val glideBigPic = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.glide_bic_pic
+        )
+        val glideLargeIcon = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.glide_large_icon
+        )
+        val bigPicStyle = NotificationCompat.BigPictureStyle()
+            .bigPicture(glideBigPic)
+            .bigLargeIcon(null)
+
+        val contentIntent = Intent(appContext, DetailActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            appContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = NotificationCompat.Builder(appContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.cloud_download)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_description))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+            .setStyle(bigPicStyle)
+            .setLargeIcon(glideLargeIcon)
+
+        notify(NOTIFICATION_ID, builder.build())
     }
 
     private fun download() {
@@ -90,6 +150,7 @@ class MainActivity : AppCompatActivity() {
         private var URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
+        private const val NOTIFICATION_ID = 0
     }
 
 }
